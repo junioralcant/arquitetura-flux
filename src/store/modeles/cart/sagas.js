845ec0dbb2eb264626d4e8ffddc34d9decfdb944
Junior Marques <junioralcant111@gmail.com>
2020-02-03 14:33:49 -0300

    Navegando no Saga

--- src/store/modeles/cart/sagas.js
+++ src/store/modeles/cart/sagas.js
@@ -1,6 +1,7 @@
 import { call, put, select, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
+import history from '../../../services/history';
 
 import { formatPrice } from '../../../util/format';
 
@@ -34,6 +35,7 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
